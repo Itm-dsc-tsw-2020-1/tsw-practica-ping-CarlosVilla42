e097953a378fa1605f0d18622a0b2306c0b42c13
CarlosVilla42 <47682711+CarlosVilla42@users.noreply.github.com>
2020-02-27 12:34:09 -0600

    Practica computadoras conectadas a la red del tec

---
+++ tsw-practica-compus_conectadas.py
@@ -0,0 +1,43 @@
+#Carlos Daniel Villa Vázquez
+#Computadoras conectadas
+import os
+red = "200.33.171.0/24"
+os.system("nmap -sn " + red)
+
+#RESULTADO!!!
+"""
+Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+Host is up (0.0037s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.023s latency).
+Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+Host is up (0.017s latency).
+Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+Host is up (0.0031s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.011s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.0039s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.0050s latency).
+Nmap scan report for 200.33.171.86
+Host is up (0.0090s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.0039s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.033s latency).
+Nmap scan report for 200.33.171.120
+Host is up (0.034s latency).
+Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+Host is up (0.022s latency).
+Nmap scan report for 200.33.171.125
+Host is up (0.019s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.011s latency).
+Nmap scan report for 200.33.171.129
+Host is up (1.6s latency).
+Nmap scan report for 200.33.171.254
+Host is up (0.012s latency).
+Nmap done: 256 IP addresses (16 hosts up) scanned in 202.86 seconds
+PS D:\carda\Descargas\Nmap>
+"""
